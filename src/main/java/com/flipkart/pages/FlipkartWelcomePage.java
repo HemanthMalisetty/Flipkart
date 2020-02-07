74bdf586a522cf1ee8fc290e29a36b39f62f53d1
HemanthMalisetty <44836897+HemanthMalisetty@users.noreply.github.com>
2020-02-07 19:05:52 +0530

    Update FlipkartWelcomePage.java

--- src/main/java/com/flipkart/pages/FlipkartWelcomePage.java
+++ src/main/java/com/flipkart/pages/FlipkartWelcomePage.java
@@ -1,12 +1,24 @@
 package com.flipkart.pages;
 
 import com.util.DriverHelper;
+import com.util.Session;
 
 public class FlipkartWelcomePage {
+    String loginPopupClose_button, electronics, mobiles;
 
-    String loginPopupClose_button = "//*[@class = '_2AkmmA _29YdH8']",
-            electronics = "//*[@class = '_1QZ6fC _3Lgyp8' and contains(text(), 'Electronics')]",
+    public FlipkartWelcomePage(){
+        if (Session.isCss()){
+            loginPopupClose_button = "[class = '_2AkmmA _29YdH8']";
+            electronics = "ul[class = '_114Zhd'] > li:nth-child(1)";
+            mobiles = "a[title = 'Mobiles']";
+        }else if (Session.isXpath()){
+            loginPopupClose_button = "//*[@class = '_2AkmmA _29YdH8']";
+            electronics = "//*[@class = '_1QZ6fC _3Lgyp8' and contains(text(), 'Electronics')]";
             mobiles = "//*[@title = 'Mobiles']";
+        }
+    }
+
+
 
     DriverHelper dh = new DriverHelper();
 
@@ -15,6 +27,7 @@ public class FlipkartWelcomePage {
     }
 
     public FlipkartWelcomePage clickOnCloseButton() {
+        dh.waitForElementToBeClickable(loginPopupClose_button);
         dh.click(loginPopupClose_button);
         return this;
     }
