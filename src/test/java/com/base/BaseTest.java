49e0b903e1b191c402d689ca3f65988ebc75cbf8
HemanthMalisetty <44836897+HemanthMalisetty@users.noreply.github.com>
2020-02-07 19:07:33 +0530

    Update BaseTest.java

--- src/test/java/com/base/BaseTest.java
+++ src/test/java/com/base/BaseTest.java
@@ -1,6 +1,7 @@
 package com.base;
 
 import com.util.LocalDriverManager;
+import com.util.Session;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -16,8 +17,8 @@ public class BaseTest {
     public WebDriver driver = null;
 
     @BeforeTest
-    @Parameters({"browser", "domain"})
-    public void beforeMethod(@Optional("chrome") String browser, @Optional("https://www.flipkart.com") String url) {
+    @Parameters({"browser", "domain", "css", "xpath"})
+    public void beforeMethod(@Optional("chrome") String browser, @Optional("https://www.flipkart.com") String url, @Optional("false") boolean css, @Optional("false") boolean xpath) {
         switch (browser.toLowerCase()) {
             case "chrome":
                 WebDriverManager.chromedriver().setup();
@@ -38,8 +39,16 @@ public class BaseTest {
         driver = LocalDriverManager.getDriver();
         driver.manage().window().maximize();
         driver.get(url);
+        Session.setCss(css);
+        Session.setXpath(xpath);
         System.out.println("Browser name : " + browser);
         System.out.println("Navigating to : " + url);
+        if (Session.isXpath()){
+            System.out.println("Executing using Xpath");
+        }else if (Session.isCss()){
+            System.out.println("Executing using CSS");
+        }
+
     }
     @AfterTest
     public void afterMethod() {
